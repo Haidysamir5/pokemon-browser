b5b11fb1260e301d1f1173e816dfcb933799f5c8
Haidysamir5 <40511123+Haidysamir5@users.noreply.github.com>
2025-08-02 12:39:00 +0300

    init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
+import Routes from './routes';
+import 'react-loading-skeleton/dist/skeleton.css';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Provide the client to your App
+    <QueryClientProvider client={queryClient}>
+      <React.Suspense>
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </React.Suspense>
+    </QueryClientProvider>
   );
 }
 
